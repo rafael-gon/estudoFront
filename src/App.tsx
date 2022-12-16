245fecd7e38e5b0d4a6201c09546a9aecc494451
Rafael Goncalves <dev@rafaelgoncalves.tech>
2022-12-16 11:57:26 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import { Card } from './components/newCard';
 import { CreditCard } from './components/CreditCard';
 import { Toggle } from './components/Toggle';
 import { Subscribe } from './components/Subscribe';
+import { Stats } from './components/stats';
 
 
 export function App() {
 
   return (
     <div className="h-screen w-screen flex justify-center items-center text-center bg-back gap-16">
-      <Card />
-      <CreditCard />
-      <Toggle />
-      <Subscribe />
+      <div className='flex flex-col gap-16'>
+        <Card />
+        <CreditCard />
+        <Stats />
+      </div>
+      <div className='flex flex-col gap-16'>
+        <Toggle />
+        <Subscribe />
+      </div>
     </div>
   )
 }
---
+++ src/components/stats.tsx
@@ -0,0 +1,18 @@
+export function Stats(){
+    return(
+        <div className="bg-white rounded-md">
+            <div>
+                <h1>santander</h1>
+                <h4 className="text-grey-400">R$576,98</h4>
+            </div>
+            <div>
+                <h1>NuBank</h1>
+                <h4 className="text-grey-400">R$465.02</h4>
+            </div>
+            <div>
+                <h1>c6</h1>
+                <h4 className="text-grey-400">R$</h4>
+            </div>
+        </div>
+    )
+}
